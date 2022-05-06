3d3182d572e9dc64f546f244945158186d4801ff
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-05-06 10:18:06 +0200

    OK : Started 07 Middleware Module, added new folder, testing adding a middleware, ..

---
+++ 07-Middleware/app.js
@@ -0,0 +1,48 @@
+
+/* We  import / declare / call  all the Node JS modules we need */
+
+// express 
+const express = require('express');
+
+
+// we declare / initialize the server 
+const app = express(); 
+
+
+
+// définition des middlewares
+function logRequest1(req, res, next) {
+	console.log("Middleware#1", " - Method ", req.method, " - URL ", req.url);
+	next();
+}
+
+
+/* Execution depends on the order of app.use */
+
+// calling middlewares
+// Middlewares are directly passed like callback functions 
+app.use(logRequest1); 
+
+
+
+app.get('/', (req, res) => {
+
+	console.log("Hello World")
+
+    // res.send sends an answer then stops the server
+	res.send("Hello World");
+});
+
+
+app.get('/hey', (req, res) => {
+
+	console.log("HO")
+
+    // res.send sends an answer then stops the server
+	res.send("HO");
+})
+
+
+
+// we serve / listen on 5001  
+app.listen(5001);
